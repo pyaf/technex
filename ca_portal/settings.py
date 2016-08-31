76dd331f50cd27a68062b4c1d5c6c43573eecd74
rishabhiitbhu <rishabh.ag342@gmail.com>
2016-08-31 17:08:51 +0530

    removed junk

--- ca_portal/settings.py
+++ ca_portal/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "2=iplcn+7@rb7np=cn5gxewwlh_ks5q&qoz6$3&4_l+^jg@3+a"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
