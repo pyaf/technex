64833cf23c67b8892bd1403e95f575832bcd71d3
rishabhiitbhu <rishabh.agrahari.eee15@iitbhu.ac.in>
2016-09-07 21:38:15 +0530

    event,college,parentevent added

--- TechnexUser/models.py
+++ TechnexUser/models.py
@@ -15,13 +15,39 @@ class UserStatus(models.Model):
     def __unicode__(self):
         return "ca : %s, techuser: %s" %(self.is_ca,self.is_techuser)
 
+class College(models.Model):
+    collegeId = models.AutoField(primary_key = True)
+    collegeName = models.CharField(max_length=250)
+    def __unicode__(self):
+        return self.collegeName
+
 
 class TechProfile(models.Model):
     user = models.OneToOneField(User)
     year = models.IntegerField(choices=year_choices)
     mobile_number = models.BigIntegerField()
-    college = models.CharField(max_length=250)
+    college = models.ForeignKey(College,null = True)
     profile_photo = models.ImageField(upload_to = get_user_image_folder,default=None)
 
     def __unicode__(self):
         return "%s %s-%s" %(self.user.first_name,self.user.last_name, self.college)
+
+class ParentEvent(models.Model):
+    parentEventId = models.AutoField(primary_key = True)
+    categoryName = models.CharField(max_length = 50)
+    description = models.TextField(null = True,blank = True)
+    def __unicode__(self):
+        return self.categoryName
+
+class Event(models.Model):
+    eventId = models.AutoField(primary_key = True)
+    eventName = models.CharField(max_length = 50)
+    parentEvent = models.ForeignKey(ParentEvent)
+    description = models.TextField(null = True,blank = True)
+    deadLine = models.DateTimeField(null = True,blank = True)
+
+class Team(models.Model):
+    teamId = models.AutoField(primary_key = True)
+    event = models.ForeignKey(Event)
+    teamLeader = models.ForeignKey(TechProfile,related_name = 'teamLeader')
+    members = models.ManyToManyField(TechProfile,related_name = 'members')
